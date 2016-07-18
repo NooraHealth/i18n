6512817d1c8ec7ea1795a90ef727971a55638004
lucyrichards <lucyannerichards@gmail.com>
2016-07-18 11:35:53 -0700

    first commit

---
+++ i18n.js
@@ -0,0 +1,5 @@
+// Write your package code here!
+
+// Variables exported by this module can be imported by other packages and
+// applications. See i18n-tests.js for an example of importing.
+export const name = 'i18n';
